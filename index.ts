be66f59585f4acdea1b4e585498950d596dd8475
Syeda Qurrat <160321850+SyedaQurrat@users.noreply.github.com>
2024-06-17 22:56:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+async function askName() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+    ]);
+
+    const upperCaseName = answers.name.toUpperCase();
+    const message = `EID MUBARAK, ${upperCaseName}!`;
+    const scriptContent = `
+        document.getElementById('greeting-button').innerText = "${message}";
+    `;
+
+    fs.writeFileSync('message.js', scriptContent);
+    console.log('message.js file has been generated with your name.');
+}
+
+askName();
